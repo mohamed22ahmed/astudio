273036835a2ce3470e97cc58027f623a2e0b6c28
mohamed22ahmed <mohammedahmedhammam113@gmail.com>
2025-03-29 15:06:15 +0200

    creating api endpoint and jobsController

---
+++ app/Http/Controllers/JobController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class JobController extends Controller
+{
+    public function index(Request $request)
+    {
+        dd('test route');
+    }
+}
